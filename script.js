cb41e850bff2452c79145813c6cf254494f50953
Jake <imhardlyjake@gmail.com>
2022-07-15 16:22:45 -0500

    Setup completed

---
+++ script.js
@@ -0,0 +1,5 @@
+// Rock Paper Scissors
+// Create Function to Randomly detirmine computers answer
+// Create variable to accept case insensitive input from human
+// Compare inputs, and display proper answer
+
